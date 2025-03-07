be12e198b151edb7d4a1e47a187310d66b1a61b4
Damian Jasek <damiannq1337@gmail.com>
2025-03-07 16:04:24 +0100

    Add files

---
+++ Model/src/main/java/org/crypto/AES.java
@@ -0,0 +1,13 @@
+package org.crypto;
+
+public class AES {
+    public AES(){
+
+    }
+    public void KeyExpansion(){
+        
+    }
+    public void encrypt(byte[] plainText) {
+
+    }
+}
--- View/src/main/java/org/example/MainController.java
+++ View/src/main/java/org/example/MainController.java
@@ -3,22 +3,48 @@ package org.example;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
+    private TextArea input;
+    @FXML
+    private TextArea output;
+    @FXML
     private Button openBtn;
-
+    @FXML
+    private Button encryptBtn;
+    @FXML
+    private Button decryptBtn;
+    @FXML
+    private Button keyBtn;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open File");
+
         openBtn.setOnAction(event -> {
             File file = fileChooser.showOpenDialog(openBtn.getScene().getWindow());
+            try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                input.setText(new String(fileInputStream.readAllBytes()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
+        encryptBtn.setOnAction(event -> {
+            output.setText(input.getText());
+        });
+        keyBtn.setOnAction(event -> {
+
+        })
     }
 }
